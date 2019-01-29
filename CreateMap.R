e8752dc81b38745ed011693567e285190ff42d13
Trevor Tombe <ttombe@ucalgary.ca>
2019-01-29 16:11:55 -0700

    gdp/capita map

--- CreateMap.R
+++ CreateMap.R
@@ -21,10 +21,11 @@ centroids<-read.csv("centroids.csv",stringsAsFactors=FALSE)
 
 # Format the data
 plotdata<-tibble(id=ca@data[,5]) %>%
+  distinct(id,Longitude,Latitude) %>%
   left_join(data,by="id") %>%
   left_join(centroids,by="id") %>%
-  distinct(id,share,Longitude,Latitude) %>%
-  mutate(Rounded=round(share*100)) %>%
+  mutate(Rounded=round(share*100),
+         gdpcap_2017=1000*(gdp_2017/cadusd_2017)/pop_2017) %>%
   filter(!(id %in% c("US-HI","US-AK")) & !is.na(share))
 
 #################
@@ -58,6 +59,38 @@ ggplot() + geom_map(data=plotdata,aes(map_id=id,fill=share),map=ca_map,color="wh
   labs(x="",y="",title="The Importance of Canada-USA Trade, by Province/State (2016)",
        subtitle="Note: Own calculations using data from Statistics Canada, Industry Canada,
 the U.S. Census Bureau, and the Bureau of Economic Analysis. Graph by @trevortombe.")
+ggsave("map.png",width=8,height=6.25,dpi=300)
+
+######################
+# GDP/Capita in 2017 #
+######################
+ggplot() + geom_map(data=plotdata,aes(map_id=id,fill=gdpcap_2017),map=ca_map,color="white") +
+  expand_limits(x=c(-130,-59),y=c(28,60)) +
+  coord_map("albers",lat0=40, lat1=60)+
+  scale_fill_continuous(low = "#eff3ff",high = "dodgerblue3") +
+  theme(
+    axis.text.y = element_blank(),
+    axis.text.x = element_blank(),
+    axis.ticks.y=element_blank(),
+    axis.ticks.x=element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(),
+    legend.position="none",
+    #legend.text=element_text(size=10),
+    plot.title = element_text(size = 16, face = "bold",hjust=0.5),
+    plot.subtitle = element_text(size = 7, color="gray50",hjust=0.5),
+    plot.margin = unit(c(-2,-2,-2,-1), "cm")
+  ) +
+  geom_text_repel(data=plotdata %>% filter(!id %in% c("US-DE","US-NH","US-RI","US-MA","US-NJ","US-MD")),
+                  aes(label = paste("$",round(gdpcap_2017),sep=""), x = Longitude, y = Latitude),
+                  point.padding = unit(0,"cm"), box.padding = unit(0.1,"cm"),fontface="bold",size=2.5) +
+  geom_text_repel(data=plotdata %>% filter(id %in% c("US-DE","US-NH","US-RI","US-MA","US-NJ","US-MD")),
+                  xlim=c(0.32,0.32),aes(label = paste("$",round(gdpcap_2017),sep=""), x = Longitude, y = Latitude),
+                  point.padding = unit(0,"cm"), box.padding = unit(0.1,"cm"),fontface="bold",size=2.5,
+                  segment.color = "gray80",segment.size = 0.25) +
+  labs(x="",y="",title="GDP per Capita in 2017, by Province/State (000s USD)",
+       subtitle="Note: Own calculations using data from Statistics Canada, the U.S. Census Bureau, and the Bureau
+of Economic Analysis. Nominal dollars, exchange rate adjusted. Graph by @trevortombe.")
 ggsave("map.png",width=8,height=6.25,dpi=300)
 
 ###############
